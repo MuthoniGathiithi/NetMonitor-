aa36f3a890f3674b1b9ba691ddddd2e863b3fae8
Joyce <gathiithijoyce74@gmail.com>
2025-07-23 22:07:09 +0300

    Add Suspicious Traffic Detection

--- netmon/main.py
+++ netmon/main.py
@@ -16,6 +16,17 @@ def tcp_packet(packet):
         print("\n📦 TCP Packet:")
         print(f"From {packet[IP].src}:{packet[TCP].sport} → {packet[IP].dst}:{packet[TCP].dport}")
 
+blacklisted_ports = [6667, 12345, 31337]  # Example suspicious ports
+
+
+def suspicious_packet(packet):
+    if packet.haslayer(TCP):
+        dport = packet[TCP].dport
+        sport = packet[TCP].sport
+        if dport in blacklisted_ports or sport in blacklisted_ports:
+            print("\n⚠️ Suspicious traffic detected!")
+            print(f"Suspicious Port: {sport} → {dport}")
+            print(f"From {packet[IP].src} to {packet[IP].dst}")
 
 # This code captures DNS queries on port 53 and prints the source and destination IP addresses along with the queried domain.
 # It uses Scapy to sniff network packets and specifically looks for DNS query requests.
